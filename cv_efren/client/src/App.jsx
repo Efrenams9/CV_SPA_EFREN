899ee64d3620146a10a7f11de5c52ef9374bea3f
Efrenams9 <efrenale5@gmail.com>
2023-11-28 15:02:56 -0600

    inicializar mi repositorio con mi proyecto en local

---
+++ cv_efren/client/src/App.jsx
@@ -0,0 +1,60 @@
+//*dependencias:
+import {Routes,Router,Switch,Route} from 'react-router-dom';
+
+//*componentes
+import "./App.css";
+import NavBar from "./Components/NavBar/NavBar";
+
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+      <div>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
+          asperiores quos vel sit odio accusantium! Laudantium incidunt
+          perferendis repellendus possimus, iusto doloremque id natus laboriosam
+          consequuntur, eum ipsam cupiditate porro. Vero laboriosam eligendi ea
+          aliquid veniam. Tempora maxime quo itaque cum? Reprehenderit, tempora
+          dolor optio unde eius fuga totam distinctio dolorem ratione facere
+          numquam dolorum est. Quia possimus eligendi minima. Architecto debitis
+          repudiandae, exercitationem reprehenderit hic deserunt dolores
+          perspiciatis laudantium impedit iusto rem assumenda inventore
+          voluptate asperiores sequi, velit aliquid unde aut veniam illo
+          suscipit enim nulla doloremque! Voluptatum, porro. Recusandae facere
+          provident modi omnis sint asperiores nihil ipsum ipsa, sequi rerum
+          quae unde non! Qui dignissimos cumque molestiae laudantium similique
+          reiciendis pariatur, consequuntur eos voluptatum odit laborum nisi
+          perferendis! Nam praesentium quibusdam alias fugit dolorem similique
+          quasi fugiat voluptatibus sed sunt, at voluptates labore cumque
+          laborum ex, recusandae voluptatum accusamus? Explicabo nisi aliquam
+          saepe nesciunt. Nesciunt sint fugit consectetur. Ipsa consequuntur
+          suscipit soluta dolorum maxime quibusdam voluptatibus sed iste
+          accusantium. Quo dolor nisi ducimus expedita recusandae tempore rem?
+          Eligendi amet dignissimos officiis est. Reprehenderit repellat tempora
+          nemo ipsam natus. Velit nam eos iure iste distinctio, nostrum sint
+          praesentium quaerat voluptatibus. Perferendis nobis sunt quam,
+          deserunt ad ipsum itaque? Quod illum sint sit aperiam odit
+          reprehenderit eligendi velit voluptas harum. Necessitatibus
+          consequuntur magni enim. Architecto, id reiciendis voluptatibus
+          numquam distinctio ducimus ullam dolore necessitatibus cumque
+          consequatur minus enim ipsum. Eum iure quaerat cum recusandae sit
+          aperiam doloribus, nesciunt nostrum expedita. Provident quod,
+          aspernatur quis, quas minima illo, veniam labore beatae est expedita
+          nulla libero quidem alias! Sapiente, quo beatae! Necessitatibus
+          mollitia repellat ipsum debitis natus cupiditate nobis libero facilis
+          pariatur! Nesciunt soluta dignissimos qui debitis natus ea corporis
+          consequatur tenetur mollitia delectus officiis numquam aperiam
+          architecto dolorum optio commodi at, explicabo voluptatibus fuga esse
+          accusamus? Quod in facilis dolorum fugiat?
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
