bc4d415cdd64ccc1b44e371e566b7107e913a157
Efrenams9 <efrenale5@gmail.com>
2023-11-30 20:44:18 -0600

    1ro 30 nov

--- cv_efren/client/src/Components/Educacion/Educacion.jsx
+++ cv_efren/client/src/Components/Educacion/Educacion.jsx
@@ -1,9 +1,33 @@
-const Educacion =()=>{
-    return (
-        <div>
-            <h2>Educacion</h2>
-        </div>
-    );
+//*dependencias
+
+//*imprtaciones
+import "./Educacion.css";
+
+import iconoJavaScript from "../../images/iconoJavaScript2.png";
+import iconopostgreSQL from "../../images/iconoPostgreSQL2.png";
+import logoCSS from "../../images/logoCSS.png";
+import logoHTML from "../../images/logoHTML.png";
+import logoNodejs from "../../images/logoNodejs.jpg";
+import logoREACT from "../../images/logoREACT.png";
+import logoREDUX from "../../images/logoREDUX2.png";
+import logoSEQUELIZE from '../../images/logoSEQUELIZE.png';
+
+const Educacion = () => {
+  return (
+    <div className="styloEducacion">
+      <h2>Educacion</h2>
+      <div className="manejoImagenes">
+        <img src={logoHTML} alt="icono logoHTML"></img>
+        <img src={logoCSS} alt="icono logoCSS"></img>
+        <img src={iconoJavaScript} alt="icono JavaScript"></img>
+        <img src={logoNodejs} alt="icono logoNodejs"></img>
+        <img src={logoREACT} alt="icono logoREACT"></img>
+        <img src={logoREDUX} alt="icono logoREDUX"></img>
+        <img src={iconopostgreSQL} alt="logo PostgreSQL "></img>
+        <img src={logoSEQUELIZE} alt="logo PostgreSQL "></img>
+      </div>
+    </div>
+  );
 };
 
 export default Educacion;
