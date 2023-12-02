93bf99ed9d72d1374dfa0b8440be8b91cd217079
Efrenams9 <efrenale5@gmail.com>
2023-12-01 19:54:53 -0600

    1ra 1-Dic-23

--- cv_efren/client/src/Components/Educacion/Educacion.jsx
+++ cv_efren/client/src/Components/Educacion/Educacion.jsx
@@ -7,7 +7,7 @@ import iconoJavaScript from "../../images/iconoJavaScript2.png";
 import iconopostgreSQL from "../../images/iconoPostgreSQL2.png";
 import logoCSS from "../../images/logoCSS.png";
 import logoHTML from "../../images/logoHTML.png";
-import logoNodejs from "../../images/logoNodejs.jpg";
+import logoNodejs from "../../images/logoNodejs.png";
 import logoREACT from "../../images/logoREACT.png";
 import logoREDUX from "../../images/logoREDUX2.png";
 import logoSEQUELIZE from '../../images/logoSEQUELIZE.png';
@@ -20,8 +20,8 @@ const Educacion = () => {
         <img src={logoHTML} alt="icono logoHTML"></img>
         <img src={logoCSS} alt="icono logoCSS"></img>
         <img src={iconoJavaScript} alt="icono JavaScript"></img>
-        <img src={logoNodejs} alt="icono logoNodejs"></img>
         <img src={logoREACT} alt="icono logoREACT"></img>
+        <img src={logoNodejs} alt="icono logoNodejs"></img>
         <img src={logoREDUX} alt="icono logoREDUX"></img>
         <img src={iconopostgreSQL} alt="logo PostgreSQL "></img>
         <img src={logoSEQUELIZE} alt="logo PostgreSQL "></img>
