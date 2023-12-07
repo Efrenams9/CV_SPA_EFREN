7ecde080cd36945d41d075781d5bd3416979d235
Efrenams9 <efrenale5@gmail.com>
2023-12-06 22:24:58 -0600

    1ra 6-nov-23

--- cv_efren/client/src/Components/Contacto/Contacto.jsx
+++ cv_efren/client/src/Components/Contacto/Contacto.jsx
@@ -12,19 +12,10 @@ const Contacto = () => {
       <div className="capa_uno">
         {/* <h2>AQUI VAN A IR LOS DATOS PARA CONTACTARME</h2> */}
         <div>
-          <h3>GMAIL</h3>
         <img src={logoGmail} alt="logo Gmail"></img>
-          <h3>LINKEDIN</h3>
         <img src={logoLinkedin} alt="logo Linkedin"></img>
-          <h3># DE TELEFONO</h3>
         <img src={logoWhatssap} alt="logo Whatssap"></img>
         </div>
-      </div>
-      <div className="logoContactos">
-        {/* <img src={} alt=""></img>
-     <img src={} alt=""></img>
-     <img src={} alt=""></img>
-     <img src={} alt=""></img> */}
       </div>
     </>
   );
